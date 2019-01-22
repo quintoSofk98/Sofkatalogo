aa04acc37934b2a87a9e2c667230fe36a01d7e27
quintoSofk98 <andres.quinto@sofka.com.co>
2019-01-22 16:10:26 -0500

    adding clients

--- clientObjects.js
+++ clientObjects.js
@@ -1,32 +1,57 @@
 "use strict";
 
 class Client {
-    constructor(name, id, type) {
+    constructor(name, type) {
         this.name = name;
-        this.id = id;
         this.type = type;
     }
 };
 
 class Enterprise extends Client {
-    constructor (name, id, type, size, sector){
-        super (name,id,type)
+    constructor (name, type, nit, size, sector){
+        super (name, type)
+        this.nit = nit;
         this.size = size;
         this.sector = sector;
+       
     }
 };
 
 class Person extends Client {
-    constructor() {
-        super(this);
+    constructor(name, type, cedula) {
+        super(name, type)
+        this.cedula = cedula;
     }
 };
 
-let alpina = new Enterprise("Alpina Productos Alimenticios S.A.", "860.025.900-2", "empresa", "5,500-10,000 empleados", "Alimenticio")
-let proteccion = new Enterprise("Protección S. A", "800.229.739-0", "empresa", "1,001-5,000 empleados", "Servicio Financiero")
-let sura = new Enterprise("Seguros Generales Suramericana S.A.", "890.903.407-9 ", "empresa", "10,001+ empleados", "Seguros")
-let coordinadora = new Enterprise("COORDINADORA MERCANTIL S.A.", "890.904.713", "empresa", "1,001-5,000 empleados", "Importaciones y Exportaciones")
+let alpina = new Enterprise("Alpina Productos Alimenticios S.A.", "empresa", "860.025.900-2", "5,500-10,000 empleados", "Alimenticio")
+let proteccion = new Enterprise("Protección S. A", "empresa",  "800.229.739-0", "1,001-5,000 empleados", "Servicio Financiero")
+let sura = new Enterprise("Seguros Generales Suramericana S.A.", "empresa", "890.903.407-9 ", "10,001+ empleados", "Seguros")
+let coordinadora = new Enterprise("COORDINADORA MERCANTIL S.A.", "empresa", "890.904.713", "1,001-5,000 empleados", "Importaciones y Exportaciones")
 
+let juan = new Person("Juan Montoya", "Persona", "1.038.435,932" )
+let liza = new Person("Liza Sanchez","Persona", "42.813.784") 
 
-console.log(alpina); 
+let clientsCollection = [alpina, proteccion, sura, coordinadora, juan, liza];
 
+Client.prototype.getClients = () => {
+    console.log('Obteniendo listado de clientes');
+    for (let i = 0; i < clientsCollection.length; i++) {
+        console.log(clientsCollection[i]);
+    }
+}
+
+Client.prototype.getClientsByType = (tipo) => {
+    console.log('Obteniendo clientes de tipo: ' + tipo);
+}
+
+Client.prototype.getClientByName = (name) => {
+    console.log('Obteniendo cliente con nombre: '+ name);
+}
+
+Client.prototype.getClientBySector = (sector) => {
+    console.log('Obteniendo clientes del sector: ' + sector);
+}
+
+let client = new Client();
+client.getClients();
