18760749382b2662252f6c2ec412ff79cbedeb81
quintoSofk98 <andres.quinto@sofka.com.co>
2019-01-23 13:59:22 -0500

    imgs

--- src/clientController/clientObjects.js
+++ src/clientController/clientObjects.js
@@ -2,18 +2,18 @@
 
 var { Enterprise, Person } = require('../models.js');
 
-let alpina = new Enterprise("Alpina Productos Alimenticios S.A.", "860.025.900-2", "5,500-10,000 empleados", "Alimenticio")
-let proteccion = new Enterprise("Protección S. A", "800.229.739-0", "1,001-5,000 empleados", "Servicio Financiero")
-let sura = new Enterprise("Seguros Generales Suramericana S.A.", "890.903.407-9 ", "10,001+ empleados", "Seguros")
-let coordinadora = new Enterprise("COORDINADORA MERCANTIL S.A.", "890.904.713", "1,001-5,000 empleados", "Importaciones y Exportaciones")
+let alpina = new Enterprise("Alpina Productos Alimenticios S.A.", "860.025.900-2", "5,500-10,000 empleados", "Alimenticio", "../imgs/clients/alpina.jpg")
+let proteccion = new Enterprise("Protección S. A", "800.229.739-0", "1,001-5,000 empleados", "Servicio Financiero", "../imgs/clients/tecnoquimicas.jpg")
+let sura = new Enterprise("Seguros Generales Suramericana S.A.", "890.903.407-9 ", "10,001+ empleados", "Seguros", "")
+let coordinadora = new Enterprise("COORDINADORA MERCANTIL S.A.", "890.904.713", "1,001-5,000 empleados", "Importaciones y Exportaciones", "")
 
-let juan = new Person("Juan Montoya", "1.038.435,932")
-let liza = new Person("Liza Sanchez", "42.813.784")
-let roxana = new Person("Roxana Gonzalez", "1.020.456.846");
-let martha = new Person("Martha Arévalo", "37.080.664");
-let gustavo = new Person("Gustavo Gómez", "1.100.950.935");
-let diego = new Person("Diego Barco", "1.053.773.244");
-let luisa = new Person("Diego Barco", "1.036.389.136");
+let juan = new Person("Juan Montoya", "1.038.435,932", "")
+let liza = new Person("Liza Sanchez", "42.813.784", "")
+let roxana = new Person("Roxana Gonzalez", "1.020.456.846", "");
+let martha = new Person("Martha Arévalo", "37.080.664", "");
+let gustavo = new Person("Gustavo Gómez", "1.100.950.935", "");
+let diego = new Person("Diego Barco", "1.053.773.244", "");
+let luisa = new Person("Diego Barco", "1.036.389.136", "");
 
 let clientsCollection = {
     enterprises: [alpina, proteccion, sura, coordinadora],
