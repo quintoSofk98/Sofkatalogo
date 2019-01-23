1f3fa90a598137b11a6df82194e8c743d4899f92
quintoSofk98 <andres.quinto@sofka.com.co>
2019-01-23 13:11:37 -0500

    search

--- src/clientController/clientFunctions.js
+++ src/clientController/clientFunctions.js
@@ -12,44 +12,24 @@ for (let i in clientList) {
 Client.prototype.getClients = () => {
     console.log('Obteniendo listado de clientes');
     for (let j = 0; j < arr.length; j++) {
-        showClient = arr[j].map(function (client) {
+        showClient += arr[j].map(function (client) {
             return '<li>' + client.name + '</li>'
         });
-
-        document.getElementById("client").innerHTML = showClient;
     }
+    document.getElementById("client").innerHTML = showClient;
 
     console.log(showClient);
 
 }
 
-Client.prototype.getClientsByType = (type) => {
+Client.prototype.getClientsPerson = (type) => {
     console.log('Obteniendo clientes de tipo: ' + type);
-    switch (type) {
-        case "Empresa":
-            var filterEnterprise = arr.filter(function (client) {
-                return (client.type === "Empresa")
-            });
-            console.log(filterEnterprise);
-            break;
-
-        case "Persona":
-            var filterPersona = arr.filter(function (client) {
-                return (client.type === "Persona")
-            });
-            console.log(filterPersona)
-            break;
-    }
-}
-
-Client.prototype.getClientByName = (name) => {
-    console.log('Obteniendo cliente con nombre: ' + name);
-
 
 }
 
-Client.prototype.getClientsBySector = (sector) => {
-    console.log('Obteniendo clientes del sector: ' + sector);
+
+Client.prototype.getClientsEnterprise = (name) => {
+
 }
 
 let client = new Client();
