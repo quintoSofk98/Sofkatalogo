990e705e2442b2a0ca2d3800adb822b36ee2e054
quintoSofk98 <andres.quinto@sofka.com.co>
2019-01-23 16:10:02 -0500

    search enterprise 3

--- src/clientController/clientFunctions.js
+++ src/clientController/clientFunctions.js
@@ -9,17 +9,17 @@ for (let i in clientList) {
     arr.push(clientList[i]);
 }
 
-Client.prototype.getClients = () => {
+(Client.prototype.getClients = () => {
     console.log('Obteniendo listado de clientes');
     for (let j = 0; j < arr.length; j++) {
         showClient += arr[j].map(function (client) {
-            return '<img src="' + client.img + '" title="' + "img" + '" alt="' + "im" + '" />';
+            return '<img src="' + client.img + '" style = "width: 200px""' + '" title="' + "img" + '" alt="' + "im" + '" />';
         });
     }
 
     document.getElementById("client").innerHTML = showClient;
 
-}
+})();
 
 Client.prototype.getClientsPerson = (type) => {
     console.log('Obteniendo clientes de tipo: ' + type);
@@ -29,26 +29,25 @@ Client.prototype.getClientsPerson = (type) => {
     console.log(filterPersona);
 }
 
-
-Client.prototype.getClientsEnterprise = () => {
+(Client.prototype.getClientsEnterprise = () => {
 
     document.getElementById("searchButton").addEventListener('click', () => {
         console.log('Obteniendo clientes de tipo: ');
         let cliSearched = document.getElementById("clientToSearch").value;
-        var show = arr[0].map( (client) => {
-            if (client.name === cliSearched) {
-                return '<img src="' + client.img + '" title="' + "img" + '" alt="' + "img" + '" />';
-            }else{
-                return '<h3>El cliente no fue encontrado<H3/>';
+        let found;
+        var show = arr[0].map((client) => {
+            found = client.name === cliSearched.toUpperCase();
+            if (found) {
+                return '<img src="' + client.img + '" style = width: 20px"' + '" title="' + "img" + '" alt="' + "img" + '" />'
+            } else {
+                return '<h3>404 - Not Found <h3/>'
             }
         });
 
         document.getElementById("client").innerHTML = show;
 
     });
-}
+})();
 
-let client = new Client();
-client.getClients();
-client.getClientsEnterprise();
+
 
--- src/clientController/clientObjects.js
+++ src/clientController/clientObjects.js
@@ -2,19 +2,19 @@
 
 var { Enterprise, Person } = require('../models.js');
 
-let alpina = new Enterprise("Alpina Productos Alimenticios S.A.", "860.025.900-2", "5,500-10,000 empleados", "Producción de alimentos", "../imgs/clients/alpina.jpg")
-let proteccion = new Enterprise("Protección S.A.", "800.229.739-0", "1,001-5,000 empleados", "Servicios Financiero", "../imgs/clients/proteccion.jpg")
-let sura = new Enterprise("SURA S.A.", "900.847.380 ", "51-200 employees", "Servicios Financieroos", "../imgs/clients/sura.png")
-let coordinadora = new Enterprise("Coordinadora Mercantil S.A.", "890.904.713", "1,001-5,000 empleados", "Importaciones y Exportaciones", "../imgs/clients/coordinadora.png")
-let alfaPeople = new Enterprise("AlfaPeople Andino S.A.", "830.013.988-9", "1,001-5,000 empleados", "Tecnología de la Información y servicios", "../imgs/clients/alfapeople.png" )
-let amadeus = new Enterprise("Amadeus.", "800-159-527", "1,001-5,000 empleados", "Tecnología de la Información y servicios", "../imgs/clients/amadeus.png")
-let exito = new Enterprise("Almacenes Exito S.A.", "890.900.608-9", "10,001+ empleados", "Comercio", "../imgs/clients/exito.png")
-let infocorp = new Enterprise("Infocorp Tecnología", "No Aplica", "51-200 empleados", "Tecnología de la Información y servicios", "../imgs/clients/infocorp.png")
-let iteriaUS = new Enterprise("iteriaUS", "No Aplica", "51-200 empleados", "Tecnología de la Información y servicios", "../imgs/clients/iteriaUS.jpg")
-let metroM = new Enterprise("Metro de Medellín Ltda", "890.923.668-1", "51-200 empleados", "Transporte", "../imgs/clients/metroM.png")
-let sas = new Enterprise("SAS", "No Aplica", "10,001+ empleados", "Tecnología de la Información y servicios", "../imgs/clients/sas.png")
-let tecnoquimicas = new Enterprise("Tecnoquimicas S.A.", "890.300.466", "5,001-10,000 empleados", "Farmacéutico", "../imgs/clients/tecnoquimicas.jpg")
-let teleperformance = new Enterprise("Teleperformance Colombia S.A.S", "900.323.853-7", "10,001+ empleados", "Telecomonicación", "../imgs/clients/teleperformance.png")
+let alpina = new Enterprise("ALPINA", "860.025.900-2", "5,500-10,000 empleados", "Producción de alimentos", "../imgs/clients/alpina.jpg");
+let proteccion = new Enterprise("PROTECCION", "800.229.739-0", "1,001-5,000 empleados", "Servicios Financiero", "../imgs/clients/proteccion.jpg");
+let sura = new Enterprise("SURA", "900.847.380 ", "51-200 employees", "Servicios Financieroos", "../imgs/clients/sura.png");
+let coordinadora = new Enterprise("COORDINADORA", "890.904.713", "1,001-5,000 empleados", "Importaciones y Exportaciones", "../imgs/clients/coordinadora.png");
+let alfaPeople = new Enterprise("ALFAPEOPLE", "830.013.988-9", "1,001-5,000 empleados", "Tecnología de la Información y servicios", "../imgs/clients/alfapeople.png" );
+let amadeus = new Enterprise("AMADEUS", "800-159-527", "1,001-5,000 empleados", "Tecnología de la Información y servicios", "../imgs/clients/amadeus.png");
+let exito = new Enterprise("EXITO", "890.900.608-9", "10,001+ empleados", "Comercio", "../imgs/clients/exito.png");
+let infocorp = new Enterprise("INFOCORP", "No Aplica", "51-200 empleados", "Tecnología de la Información y servicios", "../imgs/clients/infocorp.png");
+let iteriaUS = new Enterprise("ITERIAUS", "No Aplica", "51-200 empleados", "Tecnología de la Información y servicios", "../imgs/clients/iteriaUS.png");
+let metroM = new Enterprise("METRO", "890.923.668-1", "51-200 empleados", "Transporte", "../imgs/clients/metroM.png");
+let sas = new Enterprise("SAS", "No Aplica", "10,001+ empleados", "Tecnología de la Información y servicios", "../imgs/clients/sas.png");
+let tecnoquimicas = new Enterprise("TECNOQUIMICAS", "890.300.466", "5,001-10,000 empleados", "Farmacéutico", "../imgs/clients/tecnoquimicas.jpg");
+let teleperformance = new Enterprise("TELEFORMANCE", "900.323.853-7", "10,001+ empleados", "Telecomonicación", "../imgs/clients/teleperformance.png");
 
 let juan = new Person("Juan Montoya", "1.038.435,932", "../imgs/clients/personaHombre.png")
 let liza = new Person("Liza Sanchez", "42.813.784", "../imgs/clients/personaMujer.png")
