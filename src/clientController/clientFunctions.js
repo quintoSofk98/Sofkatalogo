1f85cbbd76d71ff4c829b592c70c8d1644fbd66b
quintoSofk98 <andres.quinto@sofka.com.co>
2019-01-23 17:00:41 -0500

    functions clients

--- src/clientController/clientFunctions.js
+++ src/clientController/clientFunctions.js
@@ -2,7 +2,7 @@
 
 var clientList = require('./clientObjects.js');
 var { Client } = require('../models.js');
-var showClient;
+var showClient = [];
 var arr = [];
 
 for (let i in clientList) {
@@ -12,13 +12,11 @@ for (let i in clientList) {
 (Client.prototype.getClients = () => {
     console.log('Obteniendo listado de clientes');
     for (let j = 0; j < arr.length; j++) {
-        showClient += arr[j].map(function (client) {
+        showClient += arr[j].map((client) => {
             return '<img src="' + client.img + '" style = "width: 200px""' + '" title="' + "img" + '" alt="' + "im" + '" />';
         });
     }
-
     document.getElementById("client").innerHTML = showClient;
-
 })();
 
 Client.prototype.getClientsPerson = (type) => {
@@ -34,17 +32,23 @@ Client.prototype.getClientsPerson = (type) => {
     document.getElementById("searchButton").addEventListener('click', () => {
         console.log('Obteniendo clientes de tipo: ');
         let cliSearched = document.getElementById("clientToSearch").value;
-        let found;
+        let foundCliente;
+        let actual;
+
         var show = arr[0].map((client) => {
-            found = client.name === cliSearched.toUpperCase();
-            if (found) {
-                return '<img src="' + client.img + '" style = width: 20px"' + '" title="' + "img" + '" alt="' + "img" + '" />'
-            } else {
-                return '<h3>404 - Not Found <h3/>'
+            foundCliente = client.name === cliSearched.toUpperCase();
+            if (foundCliente) {
+                actual = client;
             }
         });
 
-        document.getElementById("client").innerHTML = show;
+        if (actual !== undefined) {
+            document.getElementById("client").innerHTML = '<img src="' + actual.img + '" style = width: 20px"' + '" title="' + "img" + '" alt="' + "img" + '" />'
+        } else {
+            document.getElementById("client").innerHTML = "Cliente no encontrado";
+            console.log(actual);
+
+        }
 
     });
 })();
